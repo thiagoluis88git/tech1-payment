ca6426fd7fa822e72a376283f414cbaf560c26ad
Thiago Luis <thiago.luis88@gmail.com>
2024-11-25 07:51:25 -0300

    chore: adding test

--- pkg/environment/environment_test.go
+++ pkg/environment/environment_test.go
@@ -3,6 +3,7 @@ package environment_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/thiagoluis88git/tech1-payment/pkg/environment"
 	"github.com/thiagoluis88git/tech1-payment/pkg/mocks"
 )
@@ -17,5 +18,17 @@ func TestEnvironment(t *testing.T) {
 
 	t.Run("got success when initializing environment", func(t *testing.T) {
 		environment.LoadEnvironmentVariables()
+	})
+
+	t.Run("got success when initializing environment", func(t *testing.T) {
+		environment.LoadEnvironmentVariables()
+
+		assert.Equal(t, "ROOT_URL", environment.GetQRCodeGatewayRootURL())
+		assert.Equal(t, "token", environment.GetQRCodeGatewayToken())
+		assert.Equal(t, "Region", environment.GetRegion())
+		assert.Equal(t, "WEBHOOK", environment.GetWebhookMercadoLivrePaymentURL())
+		assert.Equal(t, "HOST", environment.GetMongoHost())
+		assert.Equal(t, "MongoDBName", environment.GetMongoDBName())
+		assert.Equal(t, "OrdersRoot", environment.GetOrdersRootAPI())
 	})
 }
